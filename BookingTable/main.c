9b0eda97f6fca1ed16126daf0c7bb8b7ba036e27
BHASVIC-GeorgeWilliams23 <george.williams23@students.bhasvic.ac.uk>
2023-11-06 15:35:55 +0000

    iDChecker

--- BookingTable/main.c
+++ BookingTable/main.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <process.h>
-#include <stdlib.h>
-//int available[6] = [1,1,1,1,1,1];
-char stay[50][6];
-char names[6];
-char DOB[6];
-int numGuests[6];
-int kids[6];
-int adults[6];
-char boardType[6];
-int newspaper[6];
-char bookingID[5];
-int i;
-int bookings[6][50];
+#include <string.h>
 
-int main() {
-    bookingID[0]="g";
-    char Bid[100];
-    char boardtype;
-    i=0;
-    int Tables[1][2];
-    for(i=0;i<6; i++)
-    {
-        scanf("%s",&Bid);
-        printf("\n%s",Bid);
-        if (Bid == bookingID[i])
-        {
-            printf("\n your booking ID is valid");
-            i=6;
-        }
-        else
-        {
-            printf("your booking id is not valid");
-            i=6;
+// Function to check the user's booking ID
+int bookingCheck(const char userBookingID[]) {
+    char bookingID[6][100]; // Assuming you have an array of booking IDs
+
+    // Placeholders
+    strcpy(bookingID[0], "ABC123");
+    strcpy(bookingID[1], "XYZ789");
+
+    int i;
+    for (i = 0; i < 6; i++) {
+        if (strcmp(userBookingID, bookingID[i]) == 0) {
+            printf("Your booking ID is valid\n");
+            return 1; // Return 1 to indicate a valid ID
         }
     }
 
+    printf("Your booking ID is not valid\n");
+    return 0; // Return 0 to indicate an invalid ID
 }
 
+int main() {
+    char userBookingID[100];
+
+    printf("Enter your booking ID: ");
+    scanf("%s", userBookingID);
+
+    if (bookingCheck(userBookingID)) {
+        // User's booking ID is valid
+    } else {
+        // User's booking ID is not valid
+    }
+
+    return 0;
+}
