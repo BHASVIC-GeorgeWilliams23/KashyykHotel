c9dc03b069b4d9193b51cadaa3792872b8ad85ab
BHASVIC-douglasmclaren23 <douglas.mclaren23@students.bhasvic.ac.uk>
2023-11-06 15:44:38 +0000

    gdfagj.odfh

--- BookingTable/cmake-build-debug/CMakeFiles/BookingTable.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BookingTable.
-# This may be replaced when dependencies are built.
--- BookingTable/main.c
+++ BookingTable/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
 // Function to check the user's booking ID
 int bookingCheck(const char userBookingID[]) {
     char bookingID[6][100]; // Assuming you have an array of booking IDs
-
-    // Placeholders
+// Placeholders
     strcpy(bookingID[0], "ABC123");
     strcpy(bookingID[1], "XYZ789");
-
     int i;
     for (i = 0; i < 6; i++) {
         if (strcmp(userBookingID, bookingID[i]) == 0) {
@@ -16,22 +13,17 @@ int bookingCheck(const char userBookingID[]) {
             return 1; // Return 1 to indicate a valid ID
         }
     }
-
     printf("Your booking ID is not valid\n");
     return 0; // Return 0 to indicate an invalid ID
 }
-
 int main() {
     char userBookingID[100];
-
     printf("Enter your booking ID: ");
     scanf("%s", userBookingID);
-
     if (bookingCheck(userBookingID)) {
-        // User's booking ID is valid
+// User's booking ID is valid
     } else {
-        // User's booking ID is not valid
+// User's booking ID is not valid
     }
-
     return 0;
 }
