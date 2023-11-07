8ce6993fd3d82b6ad2e9ff5602b9b7b1536c93ba
vscgeorge <gwilliams251016@gmail.com>
2023-11-07 15:10:01 +0000

    roomRates done
    calculations work, need to take in `stayLength` and `userAge`

--- checkOutTesting/main.c
+++ checkOutTesting/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <process.h>
 #include <stdlib.h>
 #include <string.h>
-//int available[6] = [1,1,1,1,1,1];
+
+// Function prototypes
+int bookingCheck(const char *userBookingID);
+int indexCheck(const char *userBookingID, const char bookingID[6][100], int arrLen);
+
+// Global arrays
 char stay[50][6];
 char names[6];
 char DOB[6];
@@ -11,49 +15,110 @@ int kids[6];
 int adults[6];
 char boardType[6];
 int newspaper[6];
-int bookingID[5];
-int i;
-int bookings[6][50];
+char bookingID[6][100]; // Array of booking IDs
+int userAge= 70;
+int stayLength = 5;
+void checkIn() {
+    printf("Checking in.\n");
+}
 
-int bookingCheck(const char userBookingID[]) {
-    char bookingID[6][100]; // Assuming you have an array of booking IDs
-// Placeholders
-    strcpy(bookingID[0], "ABC123");
+void bookTable() {
+    printf("Booking table.\n");
+}
+
+int indexCheck(const char *userBookingID, const char bookingID[6][100], int arrLen) {
+    int index = -1;
+
+    for (int i = 0; i < arrLen; i++) {
+        if (strcmp(bookingID[i], userBookingID) == 0) {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+float roomRate(const char *userBookingID) {
+    float roomCost = 0.0;
+    float boardCost = 0.0;
+    float newspaperCost = 0.0;
+    int roomRate = 0;
+    int index = indexCheck(userBookingID, bookingID, 6);
+    // ... (rest of your code here)
+    switch (index) {
+        case 0:
+            roomRate = 0;
+            printf("Room 0 does not exist.");
+            break;
+        case 1:
+            roomRate = 100;
+            break;
+        case 2:
+            roomRate = 100;
+            break;
+        case 3:
+            roomRate = 85;
+            break;
+        case 4:
+            roomRate = 75;
+            break;
+        case 5:
+            roomRate = 75;
+            break;
+        case 6:
+            roomRate = 50;
+            break;
+    }
+
+    return roomRate;
+}
+
+int bookingCheck(const char *userBookingID) {
+    // Placeholders
     strcpy(bookingID[1], "XYZ789");
-    int i;
-    for (i = 0; i < 6; i++) {
+    strcpy(bookingID[2], "123DEF");
+    strcpy(bookingID[3], "456GHI");
+    strcpy(bookingID[4], "789JKL");
+    strcpy(bookingID[5], "MNO000");
+
+    for (int i = 0; i < 6; i++) {
         if (strcmp(userBookingID, bookingID[i]) == 0) {
             printf("Your booking ID is valid\n");
             return 1; // Return 1 to indicate a valid ID
         }
     }
+
     printf("Your booking ID is not valid\n");
     return 0; // Return 0 to indicate an invalid ID
 }
 
-void checkIn(){
-    printf("Checking in.\n");
-}
-void bookTable(){
-        printf("Booking table.\n");
-}
 void checkOut() {
     char userBookingID[100];
     printf("Enter your booking ID: \n");
     scanf("%s", userBookingID);
-    bookingCheck(userBookingID);
-    if(bookingCheck(userBookingID)){
+
+    if (bookingCheck(userBookingID)) {
+        int index = indexCheck(userBookingID, bookingID, 6);
+        if (index != -1) {
+            // Perform actions for a valid booking ID
+            printf("Checking out for room %d.\n", index);
+            if(userAge<=65){
+                printf("Room rate is %f", stayLength*roomRate(userBookingID));
+            }
+            else{
+                printf("Room rate is %f", stayLength*0.9*roomRate(userBookingID));
+            }
+        }
+
 
     }
-    }
-
-
+}
 
 void mainMenu() {
     int choice;
-    printf("Welcome to the Kashyyk Hotel!\nWould you like to:\n1. Checkout\n2.Book a table in our restaurant\n3.Check in\nEnter any other number to quit.");
-    scanf("%d",&choice);
-    switch(choice){
+    printf("Welcome to the Kashyyk Hotel!\nWould you like to:\n1. Checkout\n2. Book a table in our restaurant\n3. Check in\nEnter any other number to quit.");
+    scanf("%d", &choice);
+
+    switch (choice) {
         case 1:
             checkOut();
             break;
@@ -67,6 +132,8 @@ void mainMenu() {
             break;
     }
 }
-int main(){
+
+int main() {
     mainMenu();
+    return 0; // Indicate successful program execution
 }
