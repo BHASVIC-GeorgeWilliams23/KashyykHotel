542daed0bf00cf7d74105e02a190ed7bf068c16a
BHASVIC-JosephHughes23 <145358660+BHASVIC-JosephHughes23@users.noreply.github.com>
2023-11-13 15:44:55 +0000

    almost

--- checkin/cmake-build-debug/cmake_install.cmake
+++ checkin/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/joehu/CLionProjects/checkin
+# Install script for directory: C:/Users/joehu/OneDrive/Documents/GitHub/KashyykHotel/checkin
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/joehu/CLionProjects/checkin/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/joehu/OneDrive/Documents/GitHub/KashyykHotel/checkin/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
